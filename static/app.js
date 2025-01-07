5a00ed2b4fd14f77e5084e57163a442d10f42f2a
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2025-01-07 21:33:33 +0100

    Refactor product fetching to use category selection and update event listener

--- static/app.js
+++ static/app.js
@@ -1,48 +1,85 @@
-// This is the entry point (URL and port) for our mock API
-const API = 'http://localhost:3000/';
+// // This is the entry point (URL and port) for our mock API
+// const API = 'http://localhost:3000/';
 
-// This is the mock data
-const mockData = [
-    { id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The most powerful vacuum in the world.' },
-    { id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.' },
-    { id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Deliciously overpriced chocolate.' }
-];
+// // This is the mock data
+// const mockData = [
+//     { id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The most powerful vacuum in the world.' },
+//     { id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.' },
+//     { id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Deliciously overpriced chocolate.' }
+// ];
 
-const populateProducts = async () => {
+// const populateProducts = async () => {
     
-    const products = document.querySelector('#products');
-    products.innerHTML = '';
+//     const products = document.querySelector('#products');
+//     products.innerHTML = '';
     
-    // Here we are doing a request
-    const response = await fetch(API);
+//     // Here we are doing a request
+//     const response = await fetch(API);
     
-    // Here we are taking the data from the "server" in JSON format
-    const data = await response.json();
+//     // Here we are taking the data from the "server" in JSON format
+//     const data = await response.json();
 
-    // Here we use the response data in JSON format to populate the HTML
-    for (const product of data) {
-        const item = document.createElement('product-item');
-        for (const key of ['name', 'rrp', 'info']) {
-            const span = document.createElement('span');
-            span.slot = key;
-            span.textContent = product[key];
-            item.appendChild(span);
-        }
-        products.appendChild(item);
+//     // Here we use the response data in JSON format to populate the HTML
+//     for (const product of data) {
+//         const item = document.createElement('product-item');
+//         for (const key of ['name', 'rrp', 'info']) {
+//             const span = document.createElement('span');
+//             span.slot = key;
+//             span.textContent = product[key];
+//             item.appendChild(span);
+//         }
+//         products.appendChild(item);
+//     }
+// }
+
+// document.querySelector('#fetch').addEventListener('click', async () => {
+//     await populateProducts();
+// })
+
+
+
+// customElements.define('product-item', class Item extends HTMLElement {
+//     constructor() {
+//         super()
+//         const itemTmpl = document.querySelector('#item').content.cloneNode(true)
+//         this.attachShadow({ mode: 'open' }).appendChild(itemTmpl)
+//     }
+// })
+
+const API = 'http://localhost:3000'
+
+const populateProducts = async (category) => {
+  const products = document.querySelector('#products')
+  products.innerHTML = ''
+  const res = await fetch(`${API}/${category}`)
+  const data = await res.json()
+
+  for (const product of data) {
+    const item = document.createElement('product-item')
+    for (const key of ['name', 'rrp', 'info']) {
+      const span = document.createElement('span')
+      span.slot = key
+      span.textContent = product[key]
+      item.appendChild(span)
     }
+    products.appendChild(item)
+  }
 }
 
-document.querySelector('#fetch').addEventListener('click', async () => {
-    await populateProducts();
+const category = document.querySelector('#category')
+
+category.addEventListener('input', async ({ target }) => {
+  await populateProducts(target.value)
 })
 
-
+ 
 
 customElements.define('product-item', class Item extends HTMLElement {
-    constructor() {
-        super()
-        const itemTmpl = document.querySelector('#item').content.cloneNode(true)
-        this.attachShadow({ mode: 'open' }).appendChild(itemTmpl)
-    }
+  constructor() {
+    super()
+    const itemTmpl = document.querySelector('#item').content
+    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+  }
 })
+
 
